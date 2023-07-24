04b1c7dcf37ed4abb7513745cf8702d16d40f85c
Christopher Yuan <91705030+HazeXVIII@users.noreply.github.com>
2023-07-24 05:48:16 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/idealweight', (req, res) => {
     if (gender === 'male') {
         theIdealWeight = 50 + (0.91 * (height - 152.4));
     } else if (gender === 'female') {
-        theIdealWeight = 45.5 + (0.91 * (height = 152.4));
+        theIdealWeight = 45.5 + (0.91 * (height - 152.4));
     } else {
         return res.status(400).json({error: 'Invalid gender. Please provide "male" or "female".'});
     }
@@ -44,72 +44,49 @@ app.get('/idealweight', (req, res) => {
     res.json({ idealWeight: theIdealWeight });
 });
 
+// function to calculate BMR based on gender
+function calculateBMR(weight, height, age, gender) {
+    const weightInKg = parseFloat(weight);
+    const heightInCm = parseFloat(height) * 100;
+    const ageInYears = parseFloat(age);
+
+    if (gender === 'male') {
+        return 88.362 + (13.397 * weightInKg) + (4.799 * heightInCm) - (5.677 * ageInYears);
+    } else if (gender === 'female') {
+        return 447.593 + (9.247 * weightInKg) + (3.098 * heightInCm) - (4.330 * ageInYears);
+    } else {
+        throw new Error('Invalid gender. Please provide "male" or "female".');
+    }
+}
+
+// function to calculate TDEE based on BMR and activity level
+function calculateTDEE(BMR, activityLevel) {
+    switch (activityLevel) {
+        case 'sedentary':
+            return BMR * 1.2;
+        case 'lightly_active':
+            return BMR * 1.375;
+        case 'active':
+            return BMR * 1.55;
+        case 'very_active':
+            return BMR * 1.725;
+        default:
+            throw new Error('Invalid activity level. Please provide a valid choice.');
+    }
+}
+
 app.get('/caloriesburned', async (req, res) => {
-    const { bmi, goal, weight, height, age, gender } = req.query;
-    const activityLevel = parseFloat(req.query.activityLevel);
+    const { bmi, goal, weight, height, age, gender, activityLevel} = req.query;
 
     // calculate or assign the bmi
     const calculatedBMI = bmi || (parseFloat(weight) / (parseFloat(height) * parseFloat(height)));
-    const roundedBMI = parseFloat(calculatedBMI.toFixed(2));
-
-    // calculate the basal metabolic rate (BMR) for men and women
-    let BMR;
-    if (gender === 'male') {
-        BMR = 88.362 + (13.397 * parseFloat(weight)) + (4.799 * parseFloat(height) * 100)
-        - (5.677 * parseFloat(age));
-    } else if (gender === 'female') {
-        BMR = 447.593 + (9.247 * parseFloat(weight)) + (3.098 * parseFloat(height) * 100)
-        - (4.330 * parseFloat(age));
-    } else {
-        return res.status(400).json({error: 'Invalid gender. Please provide "male" or "female".'});
-    }
-
-    // calculate the total daily expenditure (TDEE)
-    let TDEE;
-    switch (activityLevel) {
-        case 1:
-            TDEE = BMR * 1.2;
-            break;
-        case 2:
-            TDEE = BMR * 1.375;
-            break;
-        case 3:
-            TDEE = BMR * 1.55;
-            break;
-        case 4:
-            TDEE = BMR * 1.725;
-            break;
-        default:
-            return res.status(400).json({error: 'Invalid activity level. Please provide a valid choice.'})
-    }
 
     try {
-        // call the /idealweight route using axios to get idealWeight
-        const idealWeightResponse = await axios.get('http://localhost:3000/idealweight', {
-            params: {
-                height,
-                gender,
-            },
-        });
-    
-        // Extract idealWeight from the response data
-        const idealWeight = idealWeightResponse.data.idealWeight;
-    
-        // call the /bodyfat route using axios to get bodyFatPercentage
-        const bodyFatResponse = await axios.get('http://localhost:3000/bodyfat', {
-            params: {
-                bmi: roundedBMI,
-                weight,
-                height,
-                age,
-                gender,
-            },
-        });
-    
-        // extract bodyFatPercentage from the response data
-        const bodyFatPercentage = bodyFatResponse.data.bodyFat;
-    
-        // calculate calories for the selected goal
+        // Calculate BMR and TDEE
+        const BMR = calculateBMR(weight, height, age, gender);
+        const TDEE = calculateTDEE(BMR, activityLevel);
+
+        // Calculate recommendedCalories based on the selected goal
         let recommendedCalories;
         switch (goal) {
             case 'weight_loss':
@@ -122,7 +99,7 @@ app.get('/caloriesburned', async (req, res) => {
                 recommendedCalories = TDEE * 1.10; // 10% calorie surplus for weight gain
                 break;
             default:
-                return res.status(400).json({ error: 'Invalid goal. Please provide a valid goal (weight_loss, weight_maintenance, or weight_gain).' });
+                throw new Error('Invalid goal. Please provide a valid goal (weight_loss, weight_maintenance, or weight_gain).');
         }
 
         // round the recommendedCalories to two decimal places
@@ -130,11 +107,9 @@ app.get('/caloriesburned', async (req, res) => {
     
         res.json({
             goal,
-            BMI: roundedBMI,
+            BMI: calculatedBMI,
             TDEE,
             recommendedCalories,
-            idealWeight,
-            bodyFatPercentage,
         });
     } catch (error) {
         // handle any errors that may occur during the HTTP requests
--- readme.txt
+++ readme.txt
@@ -11,25 +11,26 @@ BMI Calculator: Users can calculate their BMI by entering their weight and heigh
 Body Fat Calculator: Users can calculate their Body Fat Percentage by providing their weight, height, age, and gender (optional). The calculated body fat percentage is displayed on the screen.
 Ideal Weight Calculator: Users can determine their Ideal Weight based on their height and gender. The calculated ideal weight is displayed on the screen.
 Calories Deficit Calculator: Users can estimate the number of calories burned during physical activities by specifying the activity type, weight, and duration. The calculated calories burned are displayed on the screen.
-Technologies Used:
 
+Technologies Used:
 HTML, CSS, Bootstrap: For building the user interface and styling the web page.
 JavaScript, jQuery: For handling user input, making AJAX requests to the Node.js server, and updating the web page with calculated results.
 Node.js, Express.js: For building the REST API to handle the BMI, Body Fat, Ideal Weight, and Calories Burned calculations.
 JSON: For exchanging data between the front-end and back-end.
 
 How to Use:
-Clone the repository to your local machine.
-Install Node.js if you haven't already.
-Navigate to the project directory in your terminal.
-Run npm install to install the required dependencies.
-Start the Node.js server with npm start.
-Open your web browser and access the application at http://localhost:3000.
-Enter the required information in the respective forms.
-Click the "Submit" button to perform the desired calculation.
-View the results displayed on the web page.
+1. Clone the repository to your local machine.
+2. Install Node.js if you haven't already.
+3. Navigate to the project directory in your terminal.
+4. Run `npm install` to install the required dependencies.
+5. Start the Node.js server with `npm start`.
+6. Open your web browser and access the application at http://localhost:3000.
+7. Enter the required information in the respective forms.
+8. Click the "Submit" button to perform the desired calculation.
+9. View the results displayed on the web page.
+
 Important Note:
-This application runs locally on your machine and does not require an external API or database. It is meant for personal use and learning purposes. If you plan to deploy it in a production environment, consider implementing additional security measures and validation checks.
+- This application runs locally on your machine and does not require an external API or database. It is meant for personal use and learning purposes. If you plan to deploy it in a production environment, consider implementing additional security measures and validation checks.
 
 Contributions:
 Contributions to this project are welcome! If you find any issues or have suggestions for improvements, feel free to submit a pull request or open an issue.
@@ -38,9 +39,8 @@ License:
 This project is licensed under the MIT License.
 
 Credits:
+- Created by Christopher Yuan
+- Body fat calculations based on: https://www.gaiam.com/blogs/discover/how-to-calculate-your-ideal-body-fat-percentage
+- Ideal weight calculations based on: https://pubs.asahq.org/anesthesiology/article/127/1/203/18747/Calculating-Ideal-Body-Weight-Keep-It-Simple 
 
-Created by Christopher Yuan
 Thank you for using the Fitness and Calorie Calculator! If you have any questions or feedback, please don't hesitate to contact us at cyuan81@uw.edu. We hope this tool helps you on your fitness journey!
-
-Body fat calculations based off of: https://www.gaiam.com/blogs/discover/how-to-calculate-your-ideal-body-fat-percentage
-Ideal weight calculations based off of: https://pubs.asahq.org/anesthesiology/article/127/1/203/18747/Calculating-Ideal-Body-Weight-Keep-It-Simple 
